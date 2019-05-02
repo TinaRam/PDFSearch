80cb52a8436b991046f7a0989e07efa350a07479
PPindsle <38837326+PPindsle@users.noreply.github.com>
2019-05-02 11:17:59 +0200

    JPanel

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Card extends JPanel {
 	
 	public Card() {
-		setLayout(new BorderLayout());
+		setLayout(new BorderLayout(20, 20));
 		setBackground(Color.red);
-
+		
+		JButton button = new JButton("Select directory");
+		//add(button);
 	}
 
 }
--- src/Frame.java
+++ src/Frame.java
@@ -1,18 +1,30 @@
+import java.awt.CardLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame {
+	
+	private JPanel panel;
 
 	public Frame() {
-		setSize(800, 600);
 		setTitle("Pdf file operations");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		panel = new JPanel(new CardLayout(20, 20));
+		add(panel);
+		setSize(800, 600);
+
 		setJMenuBar(new Menu(this));
 		setVisible(true);
 	}
 	
 	public void createCard() {
-		add(new Card());
+		Card card = new Card();
+
+		panel.add(card);
+		
+		card.updateUI();
 	}
 	
 }
