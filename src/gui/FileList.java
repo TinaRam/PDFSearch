7c6d9ef53432ae5fc7bb24ef387cbe117894705d
PPindsle <38837326+PPindsle@users.noreply.github.com>
2019-05-03 11:40:17 +0200

    Revert "Revert "result table""
    This reverts commit a3bb84f7b917218cf09325616e8d3b928f2eab68.

--- src/gui/FileList.java
+++ src/gui/FileList.java
@@ -1,25 +1,53 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class FileList extends JPanel {
 	
+	private SearchField search;
+	
 	public FileList(File[] fileList) {
-		SearchField search = new SearchField();
-		add(search);
+		add(search = new SearchField());
 		
 		ArrayList<File> pdfs = new ArrayList<File>();
 		
-		PDFSearch pdfSearch = new PDFSearch();
-		pdfs = pdfSearch.getPDFs(fileList);
+		pdfs = new PDFSearch().getPDFs(fileList);
 		
-		for (File f : pdfs) {
-			System.out.println(f.getName());
+		
+		
+		if (pdfs.isEmpty()) {
+			remove(search);
+			add(new JLabel("No pdfs found"), BorderLayout.SOUTH);
+		} else {
+			displayData();
+	        search.showSearch();
 		}
 		
+	}
+	
+	private void displayData() {
+		// Data to be displayed in the JTable 
+        String[][] data = { 
+            { "Kundan Kumar Jha", "CSE" }, 
+            { "Anand Jha", "IT" } 
+        }; 
+  
+        // Column Names 
+        String[] columnNames = { "Path", "Search result"}; 
+  
+        // Initializing the JTable 
+        JTable j = new JTable(data, columnNames); 
+		
+        JScrollPane sp = new JScrollPane(j); 
+        add(sp);
+        
 	}
 
 }
