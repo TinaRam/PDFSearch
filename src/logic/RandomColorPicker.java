2c8ed905f26466b61f384371fc973a9e67d53aaf
TinaHodepina <31168143+TinaRam@users.noreply.github.com>
2019-05-08 20:06:04 +0200

    s

--- src/logic/RandomColorPicker.java
+++ src/logic/RandomColorPicker.java
@@ -6,10 +6,10 @@ import java.util.Random;
 public class RandomColorPicker {
 
 	public Color getRandomColor() {
-		return new Color(getRandIntOver30(), getRandIntOver30(), getRandIntOver30());
+		return new Color(getRandIntOver100(), getRandIntOver100(), getRandIntOver100());
 	}
 
-	private int getRandIntOver30() {
+	private int getRandIntOver100() {
 		int n = new Random().nextInt(256);
 		while (n < 100) {
 			n = new Random().nextInt(256);
