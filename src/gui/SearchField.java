afc5c41b36ecd3b4aa9f16b0eb14d43b40de16d4
TinaHodepina <31168143+TinaRam@users.noreply.github.com>
2019-05-08 12:14:27 +0200

    a

--- src/gui/SearchField.java
+++ src/gui/SearchField.java
@@ -20,7 +20,7 @@ public class SearchField extends JPanel {
 
 	public SearchField(PdfPanel p) {
 		panel = p;
-		setLayout(new BorderLayout(20, 20));
+//		setLayout(new BorderLayout(20, 20));
 
 		textField = new JTextField();
 		textField.setPreferredSize(new Dimension(350, 20));
